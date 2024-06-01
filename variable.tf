66efd7dae08bf4ce37cb8c6ba0750ce00c777af8
Rishi Srinivasan <rishi.cv40@gmail.com>
2024-06-01 15:09:27 +0200

    feat: initial terraform commit

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "AzRgName" {
+  default = "AzureTfResourceGroup"
+  description = "Resource Group Name"
+}
+
+variable "AzRgLocation" {
+  default = "westus2"
+  description = "Resource Group Location"
+}
